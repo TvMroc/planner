c7200aa7bb59b1e2a502b75bd593be16a2a4eeef
TvMroc <1207837@student.roc-nijmegen.nl>
2025-07-01 09:40:46 +0200

    Setup and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, Paper, Typography } from "@mui/material"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // <Paper sx={{maxHeight: '100vh', maxWidth: '100vw', height: '98vh', padding: 0, margin: 0}}>
+    //   <Grid container spacing={3} padding={1} sx={{height: '100%', width: '100%'}}>
+    //     <Grid size={12} sx={{backgroundColor: '#606060', borderRadius: '12px', color: 'white', display: 'flex', justifyContent: 'space-between', padding: '0 0.5em'}} >
+    //      <Typography variant="h2">Planner</Typography>
+    //      <p>login</p></Grid>
+    //     <Grid size={12} container spacing={3} height={'100%'}>
+    //       <Grid size={2.5}>
+    //         <Paper sx={{width: '100%', height: '100%', backgroundColor: '#d9d9d9'}}>
+
+    //         </Paper>
+    //       </Grid>
+    //       <Grid size={9.5} container spacing={2} direction={'column'} height={'100%'}>
+    //         <Grid size={3}>
+    //           <Paper sx={{width: '100%', height: '100%', backgroundColor: '#d9d9d9'}}>
+
+    //           </Paper>
+    //         </Grid>
+    //         <Grid size={9}>
+    //           <Paper sx={{width: '100%', height: '100%', backgroundColor: '#d9d9d9'}}>
+
+    //           </Paper>
+    //         </Grid>
+    //       </Grid>
+    //     </Grid>
+    //   </Grid>
+    // </Paper>
+    <Paper sx={{maxHeight: '100vh', maxWidth: '100vw', height: '98vh', padding: 0, margin: 0}}>
+      <Grid container direction={"column"} spacing={3} padding={1} sx={{height: '100%', width: '100%'}}>
+        <Grid size={3} sx={{width: "100%", backgroundColor: '#606060', borderRadius: '12px', color: 'white', display: 'flex', justifyContent: 'space-between', padding: '0 0.5em'}} >
+         <Typography variant="h2">Planner</Typography>
+         <p>login</p></Grid>
+        <Grid size={9} container spacing={3} width={'100%'} height={'86vh'}>
+          <Grid size={3}>
+            <Paper sx={{width: '100%', height: '100%', backgroundColor: '#d9d9d9', }}></Paper>
+          </Grid>
+          <Grid size={9}>
+            <Paper sx={{width: '100%', height: '100%', backgroundColor: '#d9d9d9'}}></Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
   )
 }
 
